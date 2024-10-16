a8d0af98609ec1bca02b592292d3697a973d7675
mschmitt <mschmitt@mri-inc.net>
2024-10-16 13:43:27 -0400

    Dap progress

--- lua/configs/dapconfig.lua
+++ lua/configs/dapconfig.lua
@@ -1,18 +1,35 @@
+-- This is my reference
+-- https://github.com/Wiebesiek/ZeoVim
+
+
 local dap = require "dap"
 
 dap.adapters.coreclr = {
   type = "executable",
-  command = "C:/Users/mschmitt/AppData/Local/nvim-data/mason/bin/netcoredbg.cmd",
+  command = vim.fs.normalize(vim.fn.stdpath('data') .. "/mason/packages/netcoredbg/netcoredbg/netcoredbg.exe"),
   args = {"--interpreter=vscode"},
 }
 
 dap.configurations.cs = {
   {
+    justMyCode = false,
+    stopAtEntry = false,
     type = "coreclr",
     name = "launch - netcoredbg",
     request = "launch",
     program = function()
-      return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/net*/", "file")
+      return vim.fn.input("Path to dll: ", vim.fs.normalize(vim.fn.getcwd()) .. "/bin/Debug/net8.0/", "file")
+    end,
+    env = {
+      ASPNETCORE_ENVIRONMENT = function()
+        return vim.fn.input("ASPNETCORE_ENVIRONMENT: ", "Development")
+      end,
+      ASPNETCORE_URLS = function()
+        return "http://localhost:5050"
+      end,
+    },
+    cwd = function()
+      return vim.fn.input("Workspace Folder: ", vim.fn.getcwd() .. "/", "file")
     end,
   },
 }
