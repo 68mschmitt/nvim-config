867c0cb49d14a0350317cd4b43317069015795b3
Mike Schmitt <68mschmitt@gmail.com>
2024-10-22 20:57:35 -0400

    Working dap

--- lua/configs/dapconfig.lua
+++ lua/configs/dapconfig.lua
@@ -2,34 +2,198 @@
 -- https://github.com/Wiebesiek/ZeoVim
 
 
-local dap = require "dap"
+local status, dap = pcall(require, 'dap')
+if (not status) then
+  print('Dap not loaded')
+  return
+end
+
+local dap_status, dapui = pcall(require, 'dapui')
+if (not dap_status) then
+	print("dapui not loaded")
+	return
+end
+
+local ph_status, dotnet_ph = pcall(require, 'utilities.path_finder')
+if (not ph_status) then
+  print('path_finder not loaded')
+  return
+end
 
 dap.adapters.coreclr = {
   type = "executable",
-  command = vim.fs.normalize(vim.fn.stdpath('data') .. "/mason/packages/netcoredbg/netcoredbg/netcoredbg.exe"),
+  command = dotnet_ph.GetNetCoreDbgPath(),
   args = {"--interpreter=vscode"},
+  options = {
+    detatched = false,
+    cwd = dotnet_ph.GetDebugDwd
+  },
 }
+
+dap.adapters.netcoredbg = vim.deepcopy(dap.adapters.coreclr)
 
 dap.configurations.cs = {
   {
-    justMyCode = false,
-    stopAtEntry = false,
     type = "coreclr",
     name = "launch - netcoredbg",
     request = "launch",
-    program = function()
-      return vim.fn.input("Path to dll: ", vim.fs.normalize(vim.fn.getcwd()) .. "/bin/Debug/net8.0/", "file")
-    end,
-    env = {
-      ASPNETCORE_ENVIRONMENT = function()
-        return vim.fn.input("ASPNETCORE_ENVIRONMENT: ", "Development")
-      end,
-      ASPNETCORE_URLS = function()
-        return "http://localhost:5050"
-      end,
-    },
-    cwd = function()
-      return vim.fn.input("Workspace Folder: ", vim.fn.getcwd() .. "/", "file")
-    end,
+    program = dotnet_ph.GetDllPath,
+    console = "integratedTerminal"
+  }
+}
+
+local status_ui, dap_ui = pcall(require, 'dapui')
+if (not status_ui) then
+  print("dapui not loaded")
+  return
+end
+
+dap_ui.setup({
+  controls = {
+    element = "repl",
+    enabled = true,
+    icons = {
+      disconnect = "",
+			pause = "",
+			play = "",
+			run_last = "",
+			step_back = "",
+			step_into = "",
+			step_out = "",
+			step_over = "",
+			terminate = ""
+    }
   },
+  element_mappings = {},
+  expand_lines = true,
+  floating = {
+    border = "single",
+    mappings = {
+      close = { "q", "<Esc>" }
+    }
+  },
+  force_buffers = true,
+  icons = {
+    collapsed = "",
+		current_frame = "",
+		expanded = ""
+  },
+  layouts = {
+    {
+      elements = {
+        {
+          id = "console",
+          size = 0.2
+        },
+        {
+          id = "breakpoints",
+          size = 0.2
+        },
+        {
+          id = "stacks",
+          size = 0.2
+        },
+        {
+          id = "repl",
+          size = 0.2
+        },
+        {
+          id = "watches",
+          size = 0.2
+        },
+      },
+      position = "left",
+      size = 50
+    },
+    {
+      elements = {
+        {
+          id = "scopes",
+          size = 1
+        }
+      },
+      position = "bottom",
+      size = 10
+    }
+  },
+  mappings = {
+    edit = "e",
+    expand = { "<CR>", "<2-LeftMouse>" },
+    open = "o",
+    remove = "d",
+    repl = "r",
+    toggle = "t"
+  },
+  render = {
+    indent = 1,
+    max_value_lines = 100
+  }
+})
+
+dap.listeners.after.event_initialized["dapui_config"] = function()
+	dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+	dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+	dapui.close()
+end
+
+vim.fn.sign_define('DapBreakpoint', { text = '🟥', texthl = '', linehl = '', numhl = '' })
+vim.fn.sign_define('DapStopped', { text = '▶️', texthl = '', linehl = '', numhl = '' })
+
+------------
+-- PYTHON --
+------------
+dap.adapters.python = function(cb, config_py)
+	if config_py.request == 'attach' then
+		---@diagnostic disable-next-line: undefined-field
+		local port = (config_py.connect or config_py).port
+		---@diagnostic disable-next-line: undefined-field
+		local host = (config_py.connect or config_py).host or '127.0.0.1'
+		cb({
+			type = 'server',
+			port = assert(port, '`connect.port` is required for a python `attach` configuration'),
+			host = host,
+			options = {
+				source_filetype = 'python',
+			},
+		})
+	else
+		cb({
+			type = 'executable',
+			command = vim.fs.normalize(vim.fn.stdpath('data') .. '/mason/packages/debugpy/venv/Scripts/python'),
+			args = { '-m', 'debugpy.adapter' },
+			options = {
+				source_filetype = 'python',
+			},
+		})
+	end
+end
+dap.configurations.python = {
+	{
+		-- The first three options are required by nvim-dap
+		type = 'python', -- the type here established the link to the adapter definition: `dap.adapters.python`
+		request = 'launch',
+		name = "Launch file",
+		console = "integratedTerminal",
+		-- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
+
+		program = "${file}", -- This configuration will launch the current file if used.
+		pythonPath = function()
+			-- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
+			-- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
+			-- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
+			-- local cwd = vim.fn.getcwd()
+			-- if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
+			--   return cwd .. '/venv/bin/python'
+			-- elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
+			--   return cwd .. '/.venv/bin/python'
+			-- else
+			-- return 'C:\\Windows\\py.exe'
+			return 'C:\\Windows\\py.exe'
+			--end
+		end,
+	},
 }
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -14,19 +14,53 @@ return {
   },
 
   {
-    "mfussenegger/nvim-dap",
-    config = function()
-      require "configs.dapconfig"
+		"mfussenegger/nvim-dap",
+		lazy = true,
+		dependencies = {
+			"jay-babu/mason-nvim-dap.nvim",
+			"rcarriga/nvim-dap-ui",
+		},
+		config = function()
+			require("configs.dapconfig")
+		end,
+	},
+	{
+		"rcarriga/nvim-dap-ui",
+		lazy = true,
+    dependencies = {
+      "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"
+    }
+	},
+	{
+		"jay-babu/mason-nvim-dap.nvim",
+		lazy = true,
+		opts = {
+			ensure_installed = { "python", "coreclr", "node2", "js", "chrome" }
+		},
+	},
+
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' },
+  },
+
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
     end,
   },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+       "html", "css"
+  		},
+  	},
+  },
 }
